8b184bb3b12701ceca2e0691db53596e3f1f6254
Ester Carnassier <imwonderchild@gmail.com>
2024-01-10 19:03:08 +0600

    training dao, training service slightly changed

--- src/main/java/cz/cvut/ear/clubevidence/service/TrainingService.java
+++ src/main/java/cz/cvut/ear/clubevidence/service/TrainingService.java
@@ -1,4 +1,23 @@
 package cz.cvut.ear.clubevidence.service;
 
+import cz.cvut.ear.clubevidence.dao.TrainingDao;
+import cz.cvut.ear.clubevidence.model.Training;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class TrainingService {
+    private final TrainingDao trainingDao;
+    @Autowired
+    public TrainingService(TrainingDao trainingDao) {
+        this.trainingDao = trainingDao;
+    }
+    @Transactional(readOnly = true)
+    public List<Training> findAll() {
+        return trainingDao.findAll();
+    }
+
 }
